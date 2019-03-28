a7281bfd7f44da1a64c1cb88511f67fd77c03273
Alisson Boucinhas <alissonboucinhas13@hotmail.com>
2019-03-27 22:13:19 -0300

    commit inicial

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,40 @@
 import { BrowserModule } from '@angular/platform-browser';
+import { HttpClientModule } from '@angular/common/http';
+import { ReactiveFormsModule } from '@angular/forms';
 import { NgModule } from '@angular/core';
 
-import { AppRoutingModule } from './app-routing.module';
+import { ModalModule } from 'ngx-bootstrap/modal';
+
 import { AppComponent } from './app.component';
+import { AppRoutingModule } from './app-routing.module';
+
+import { HeaderComponent } from './core/components/header/header.component';
+import { FooterComponent } from './core/components/footer/footer.component';
+
+import { ToolsComponent } from './tools/tools.component';
+import { CardToolComponent } from './tools/card-tool/card-tool.component';
+import { SearchToolsComponent } from './tools/search-tools/search-tools.component';
+import { ModalAddToolComponent } from './tools/modal-add-tool/modal-add-tool.component';
+import { ModalRemoveToolComponent } from './tools/card-tool/modal-remove-tool/modal-remove-tool.component';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ToolsComponent,
+    HeaderComponent,
+    FooterComponent,
+    SearchToolsComponent,
+    CardToolComponent,
+    ModalAddToolComponent,
+    ModalRemoveToolComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule,
+    ReactiveFormsModule,
+    ModalModule.forRoot()
   ],
   providers: [],
   bootstrap: [AppComponent]
